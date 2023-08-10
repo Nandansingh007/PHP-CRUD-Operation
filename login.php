c0760a35c95e9c3874073b00226cd39a73ee98ef
nandan <nandan.singhs007@gmail.com>
2023-08-10 11:17:47 +0545

    Updated

--- login.php
+++ login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +9,7 @@
 </head>
 <body>
     <h2>Login</h2>
-    <form action="authenticate.php" method="post">
+    <form action="auth.php" method="post">
         <input type="text" name="username" placeholder="Username" required>
         <input type="password" name="password" placeholder="Password" required>
         <button type="submit" name="login">Login</button>
